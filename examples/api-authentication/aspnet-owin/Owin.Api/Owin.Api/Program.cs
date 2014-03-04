215a1a1e51b44cc4db3e98dab7fd15b911143fc2
Matias Woloski <matiasw@gmail.com>
2014-03-04 13:51:02 -0800

    add owin sample

---
+++ examples/api-authentication/aspnet-owin/Owin.Api/Owin.Api/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Owin.Api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start OWIN host 
+            var server = WebApp.Start<Startup>(url: "http://localhost:1337/");
+            Console.WriteLine("Web API listening at http://localhost:1337/");
+            Console.WriteLine("Press ENTER to terminate");
+            Console.ReadLine(); 
+        }
+    }
+}
