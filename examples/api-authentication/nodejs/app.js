0b75fac49bb6be6c0b0479364d78aba588bbb519
Alberto Pose <albertopose@gmail.com>
2014-03-04 16:04:34 -0300

    Removing custom-signup code from api-authentication example.

--- examples/api-authentication/nodejs/app.js
+++ examples/api-authentication/nodejs/app.js
@@ -20,37 +20,5 @@ app.get('/api/protected', function (req, res) {
   res.send(200, 'This API Rocks!');
 });
 
-/** For custom signup example **/
-
-var Auth0 = require('auth0');
-var extend = require('xtend');
-
-var api = new Auth0({
-  domain:       'contoso.auth0.com',
-  clientID:     'DyG9nCwIEofSy66QM3oo5xU6NFs3TmvT',
-  clientSecret: SECRET
-});
-
-var CONNECTION = 'Username-Password-Authentication';
-
-app.use(express.bodyParser());
-
-app.use('/custom-signup', function (req, res) {
-  var data = extend(req.body, {connection: CONNECTION, email_verified: false});
-
-  api.createUser(data, function (err) {
-    if (err) {
-      console.log('Error creating user: ' + err);
-      res.send(500, err);
-      return;
-    }
-
-    res.send(200);
-    return;
-  });
-});
-
-/** ------------------------ **/
-
 app.listen(1337);
 console.log('listening on port http://localhost:1337');
