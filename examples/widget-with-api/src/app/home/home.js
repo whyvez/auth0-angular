3918793f6854cdd2c9ddf59e1228d109357a57d4
Martin Gontovnikas <martin@gon.to>
2014-07-30 12:24:15 -0300

    Calling API ok

--- examples/widget-with-api/src/app/home/home.js
+++ examples/widget-with-api/src/app/home/home.js
@@ -1,12 +1,20 @@
 angular.module( 'sample.home', [
-  
+
 ])
-.controller( 'HomeCtrl', function HomeController( $scope, auth ) {
+.controller( 'HomeCtrl', function HomeController( $scope, auth, $http ) {
 
   $scope.auth = auth;
 
   $scope.callApi = function() {
     // Just call the API as you'd do using $http
+    $http({
+      url: 'http://localhost:3001/secured/ping',
+      method: 'GET'
+    }).then(function() {
+      alert("We got the secured data successfully");
+    }, function() {
+      alert("Please download the API seed so that you can call it.");
+    });
   }
 
 });
