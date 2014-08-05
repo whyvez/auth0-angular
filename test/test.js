949974e5f41a473ba3c2dac5527d716201e2604a
Martin Gontovnikas <martin@gon.to>
2014-08-05 13:53:46 -0300

    Added more tests for delegation

--- test/test.js
+++ test/test.js
@@ -218,7 +218,7 @@ describe('Auth0 Angular', function () {
   });
 
   describe('Token store', function () {
-    var auth, $rootScope, $timeout, getDelegationToken, hasTokenExpiredOriginal;
+    var auth, $rootScope, $timeout, getDelegationToken, hasTokenExpiredOriginal, refreshToken, renewIdToken;
 
     beforeEach(initAuth0);
 
@@ -236,8 +236,15 @@ describe('Auth0 Angular', function () {
       token = token.join('.');
       getDelegationToken = sinon.stub();
       getDelegationToken.onCall(0).callsArgWith(1, null, {id_token: token});
+
+      refreshToken = sinon.stub();
+      refreshToken.onCall(0).callsArgWith(1, null, {id_token: token});
+      renewIdToken = sinon.stub();
+      renewIdToken.onCall(0).callsArgWith(1, null, {id_token: token});
       auth.config.auth0js = {
         getDelegationToken: getDelegationToken,
+        refreshToken: refreshToken,
+        renewIdToken: renewIdToken,
         getProfile: function() {}
       };
     }));
@@ -248,6 +255,28 @@ describe('Auth0 Angular', function () {
 
     it('should get the token using the API on the first time', function (done) {
       auth.getToken('client-id').then(function (token) {
+        expect(token).to.be.ok;
+      })
+      .then(done);
+
+      // Flush the promise and timeout!
+      $rootScope.$apply();
+      $timeout.flush();
+    });
+
+    it('should refresh the token OK', function (done) {
+      auth.refreshToken('refresh_token').then(function (token) {
+        expect(token).to.be.ok;
+      })
+      .then(done);
+
+      // Flush the promise and timeout!
+      $rootScope.$apply();
+      $timeout.flush();
+    });
+
+    it('should renew the id_token OK', function (done) {
+      auth.renewIdToken().then(function (token) {
         expect(token).to.be.ok;
       })
       .then(done);
