8bf520d21ca38a7fdd5c8a0ed18f8e971fb63afb
Martin Gontovnikas <martin@gon.to>
2014-08-22 12:38:23 -0300

    Added check for email

--- src/auth0-angular.js
+++ src/auth0-angular.js
@@ -380,7 +380,7 @@
 
       var checkHandlers = function(options) {
         var successHandlers = getHandlers('loginSuccess');
-        if (!options.popup && (!options.username || !options.email) && (!successHandlers || successHandlers.length === 0)) {
+        if (!options.popup && !options.username && !options.email && (!successHandlers || successHandlers.length === 0)) {
             throw new Error('You must define a loginSuccess handler ' +
               'if not using popup mode or not doing ro call because that means you are doing a redirect');
           }
