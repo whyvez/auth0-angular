f92a6f8ee481b3c3912a00413bf9469533099a35
Martin Gontovnikas <martin@gon.to>
2014-10-09 19:04:08 -0300

    Fixed refresh token example

--- examples/refresh-token/app.js
+++ examples/refresh-token/app.js
@@ -2,9 +2,12 @@ angular.module( 'sample', [
   'auth0',
   'ngRoute',
   'sample.home',
-  'sample.login'
+  'sample.login',
+  'angular-storage',
+  'angular-jwt'
 ])
-.config( function myAppConfig ( $routeProvider, authProvider, $httpProvider, $locationProvider) {
+.config( function myAppConfig ( $routeProvider, authProvider, $httpProvider,
+  $locationProvider, jwtInterceptorProvider) {
   $routeProvider
     .when( '/', {
       controller: 'HomeCtrl',
@@ -22,22 +25,43 @@ angular.module( 'sample', [
   authProvider.init({
     domain: AUTH0_DOMAIN,
     clientID: AUTH0_CLIENT_ID,
-    callbackURL: location.href,
     loginUrl: '/login'
   });
 
-  authProvider.on('loginSuccess', function($location) {
+  authProvider.on('loginSuccess', function($location, profilePromise, idToken, refreshToken) {
     $location.path('/');
+    profilePromise.then(function(profile) {
+      store.set('profile', profile);
+      store.set('token', idToken);
+      store.set('refreshToken', refreshToken);
+    });
   });
 
   authProvider.on('loginFailure', function($log, error) {
     $log('Error logging in', error);
   });
 
-  $httpProvider.interceptors.push('authInterceptor');
-})
-.run(function(auth) {
-  auth.hookEvents();
+  jwtInterceptorProvider.tokenGetter = function(store, $http, jwtHelper) {
+    var idToken = store.get('token');
+    var refreshToken = store.get('refreshToken');
+    if (jwtHelper.isTokenExpired(idToken)) {
+      return auth.refreshIdToken(refreshToken);
+    } else {
+      return idToken;
+    }
+  }
+
+  $httpProvider.interceptors.push('jwtInterceptor');
+}).run(function($rootScope, auth, store) {
+  $rootScope.$on('$locationChangeStart', function() {
+    if (!auth.isAuthenticated) {
+      var token = store.get('token');
+      if (token) {
+        auth.authenticate(store.get('profile'), token);
+      }
+    }
+
+  });
 })
 .controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
   $scope.$on('$routeChangeSuccess', function(e, nextRoute){
